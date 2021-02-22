cd2f37ea792e395a974739f78416f5aba8aa4a61
unknown <weeslleey354@gmail.com>
2021-02-22 20:29:43 -0300

    finish first class 01

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,13 @@ import express from 'express'
 
 const app = express()
 
+
+app.get("/", (request, response) => {
+    return response.json({ message: "Bem vindo" })
+})
+
+app.post("/", (request, response) => {
+    return response.json({ message: "Os dados foram salvos com sucesso" })
+})
+
 app.listen(3333, () => console.log("Server is running"))
