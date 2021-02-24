792099ca8034e0541714957d6f01f279c4d78871
unknown <weeslleey354@gmail.com>
2021-02-23 22:57:08 -0300

    create typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
+import 'reflect-metadata'
 import express from 'express'
+import "./database"
 
 const app = express()
-
 
 app.get("/", (request, response) => {
     return response.json({ message: "Bem vindo" })
