5fae6ed4dad0acf00a2085c728073ec0e6b6fcfa
unknown <weeslleey354@gmail.com>
2021-02-26 22:36:46 -0300

    ajustes

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,3 @@
-
 import app from './app'
 
 app.listen(3333, () => console.log("Server is running! 3333 📌"))
--- src/services/SendMailService.ts
+++ src/services/SendMailService.ts
@@ -1,45 +1,64 @@
-import nodemailer, { Transporter } from "nodemailer"
-import fs from "fs"
-import handlebars from "handlebars"
+import nodemailer, { Transporter } from 'nodemailer'
+import handlebars from 'handlebars'
+import fs from 'fs'
 
-class SendMailService {
+interface SendMailData {
+    to: string;
+
+    subject: string;
+
+    variables: object;
+
+    patch: string;
+}
+
+
+class SendMailServices {
 
     private client: Transporter
 
     constructor() {
-        nodemailer.createTestAccount().then(account => {
-            const transporter = nodemailer.createTransport({
-                host: account.smtp.host,
-                port: account.smtp.port,
-                secure: account.smtp.secure,
-                auth: {
-                    user: account.user,
-                    pass: account.pass
-                }
-            })
+        nodemailer.createTestAccount().then(
+            account => {
+                const transporter = nodemailer.createTransport({
+                    host: account.smtp.host,
+                    port: account.smtp.port,
+                    secure: account.smtp.secure,
+                    auth: {
+                        user: account.user,
+                        pass: account.pass
+                    }
+                });
 
-            this.client = transporter
-        })
+                this.client = transporter
+            }
+        )
 
     }
 
-    async execute(to: string, subject: string, variables: object, path: string) {
-        const templateFileContent = fs.readFileSync(path).toString("utf8")
+    async execute({ to, subject, variables, patch }: SendMailData) {
+
+        const templateFileContent = fs.readFileSync(patch).toString("utf8")
+
         const mailTemplateParse = handlebars.compile(templateFileContent)
 
         const html = mailTemplateParse(variables)
-
 
         const message = await this.client.sendMail({
             to,
             subject,
             html,
-            from: "NPS <noreply@nps.com.br>"
+            from: "NPS <noreplay@nps.com.br>"
+
         })
 
-        console.log(`Mensagem enviada: ${message.messageId}`)
-        console.log(`URL de preview: ${nodemailer.getTestMessageUrl(message)}`)
-    }
+        console.log('Message sent: %s', message.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+    };
+
 }
 
-export default new SendMailService()
+
+
+
+export default new SendMailServices;
